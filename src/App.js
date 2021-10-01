76423ef88e87a84526700cf8a314dd361f6f3b72
Kostya Huk <fozzy2@ua.fm>
2021-10-02 02:37:40 +0300

    added router and documentation page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import DocumentationPage from "./pages/Documentation";
+
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Switch>
+          <Route path={'/documentation'} component={DocumentationPage}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Documentation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function DocumentationPage () {
+    return (<div>
+        this is documentation
+    </div>);
+}
+
+export default DocumentationPage
