4526ec28ed32a9b14f58f4b37d6f3b20554ac0d1
web <web@training.workrocks.ua>
2021-11-23 20:56:56 +0200

    new_task

--- src/pages/new_task/NewTask.js
+++ src/pages/new_task/NewTask.js
@@ -7,19 +7,20 @@ import Button from "../../components/button/button";
 
 
 function NewTask() {
-    const [radioVal, setRadioVal] = useState('');
+    const [radioVal1, setRadioVal1 ] = useState('');
+    const [radioVal2, setRadioVal2 ] = useState('');
     const [selectVal, setSelectVal] = useState('');
 
     return (
         <div className="New Task">
             <h2>Новое задание</h2>
             <div className="Radio">
-                    <Radio name={'RadioButton'} checked={radioVal === 'val1'} label={'Экипаж'} value={'val1'} onChange={(val)=>{setRadioVal(val)}}></Radio>
-                    <Radio name={'RadioButton'} checked={radioVal === 'val2'} label={'Индивидуально'} value={'val2'} onChange={(val)=>{setRadioVal(val)}}></Radio>
+                    <Radio name={'RadioButton1'} checked={radioVal1 === 'поисковая группа'} label={'Экипаж'} value={'поисковая группа'} onChange={(val)=>{setRadioVal1(val)}}></Radio>
+                    <Radio name={'RadioButton1'} checked={radioVal1 === 'один человек'} label={'Индивидуально'} value={'один человек'} onChange={(val)=>{setRadioVal1(val)}}></Radio>
             </div>
             <div className="Select">
                     <Select
-                            label={'Select Form'}
+                            label={'Select '}
                             value={selectVal}
                             options={[{label: 'ФИО1', value:'value1'}, {label: 'ФИО2', value:'value2'}]}
                             onChange={(val)=>{setSelectVal(val)}}>
@@ -27,14 +28,14 @@ function NewTask() {
                     </Select>
             </div>
             <div className="Radio" >
-                    <Radio name={'RadioButton'} checked={radioVal === 'val3'} label={'Область'} value={'val3'} onChange={(val)=>{setRadioVal(val)}}></Radio>
-                    <Radio name={'RadioButton'} checked={radioVal === 'val4'} label={'Путь'} value={'val4'} onChange={(val)=>{setRadioVal(val)}}></Radio>
+                    <Radio name={'RadioButton2'} checked={radioVal2 === 'область карты'} label={'Область'} value={'область карты'} onChange={(val)=>{setRadioVal2(val)}}></Radio>
+                    <Radio name={'RadioButton2'} checked={radioVal2 === 'маршрут'} label={'Путь'} value={'маршрут'} onChange={(val)=>{setRadioVal2(val)}}></Radio>
             </div>
-            <div>
+            <div className="Map">
                     <p> ТУТ БУДЕТ КАРТА</p>
             </div>
             <div className="Button">
-                    <Button value={'Применить'} onClick={()=>{alert('button is clicked')}}></Button>
+                    <Button value={'Применить'} onClick={()=>{alert('обработка данных')}}></Button>
             </div>
 
         </div>
