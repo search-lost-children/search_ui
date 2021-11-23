f6bb68802fa14cafc53605d9c0970553889efa9d
web <web@training.workrocks.ua>
2021-11-23 20:58:22 +0200

    Great job keep it up

--- src/pages/Coordinators.js
+++ src/pages/Coordinators.js
@@ -14,63 +14,88 @@ import * as axios from "axios";
 import GridTable from "@nadavshaar/react-grid-table";
 
 
-
-// function postData(firstName, SecondName){
-//     axios.post('/user', {
-//         firstName: ,
-//         lastName: ''
-//     })
-//         .tahen(function (response) {
-//             console.log(response);
-//         })
-//         .catch(function (error) {
-//             console.log(error);
-//         });
-// }
-// function deleteData(){
-//
-// }
-
-
-
-
 function Coordinators() {
     let location = useLocation()
     let history = useHistory()
     let match = useRouteMatch()
+    debugger
+    const [selectVal, setSelectVal] = useState('')
+    const [participantsVal, setParticipantsVal] = useState([])
+    const [coordinatorsVal, setCoordinatorsVal] = useState([])
+    let idksomevar = [
+        {id: '777', firstName: 'James', lastName:'Raynor'},
+        {id: '122', firstName: 'Sara', lastName:'Kerrigan'}]
 
-    const idksomevar = [{label: 'это лейбл 1', value:'value1'}, {label: 'это лейбл 2', value:'value2'}]
+    function postData(firstName, SecondName){
+        axios.post('/api/v1/searches/:id/coordinators', {
 
-    useEffect(() => {
-        axios.get('example.com')
-            .then(function (idksomevar) {
-              // setOptionsVal(response)
-                setOptionsVal(idksomevar)
+        })
+            .tahen(function (response) {
+                console.log(response);
             })
             .catch(function (error) {
-               console.log('error occured')
+                console.log(error);
+            });
+}
+function deleteData(){
+
+}
+
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/v1/searches/:id/participants')
+            .then(function (response) {
+                // setParticipantsVal(response.data)
+            })
+            .catch(function (error) {
+                setCoordinatorsVal(idksomevar)
             })
             .then(function () {
                 // always executed
             });
-    });
+    }, [match.params.id]);
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/v1/searches/:id/coordinators')
+            .then(function (response) {
+                //  setCoordinatorsVal(response.data)
+                setCoordinatorsVal(idksomevar)
+            })
+            .catch(function (error) {
+                setCoordinatorsVal(idksomevar)
+            })
+            .then(function () {
+                // always executed
+            });
+    }, [match.params.id]);
+
+    function filtering() {
+        let sortedListOfParticipants = []
+        for (let i = 0; i < participantsVal.length; i++) {
+            if (!coordinatorsVal.find((el) => el.id === participantsVal[i].id)) {
+                sortedListOfParticipants.push(participantsVal[i])
+            }
+        }
+        return sortedListOfParticipants.map((el) => ({
+            value: el.id,
+            label: el.lastName
+        }))
+    }
 
 
-    const [selectVal, setSelectVal] = useState('')
-    const [optionsVal, setOptionsVal] = useState([])
-    let searchers = []
+    let selectOptions = filtering()
 
     return (
         <div className="?????????????????????????????????????">
-            <h1> Координаторы поиска [bgn'x[skgb </h1>
+            <h1> Координаторы поиска ?????????? </h1>
             <div>
                 <Select
                     label={'Select Form'}
                     value={selectVal}
-                    options={optionsVal}
+                    options={selectOptions}
                     onChange={(val)=>{setSelectVal(val)}}/>
                 <Button
-                    onClick = {(selectVar) => searchers.push(selectVar)}
+                    // onClick = {(selectVar) => .push(selectVar)}
+
                     value={"Add"}
                 >
                     Добавить
