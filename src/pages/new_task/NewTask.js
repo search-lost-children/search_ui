3ebdabcde84d8862287e4acebe87c0ab52ee1db9
web <web@training.workrocks.ua>
2021-11-30 20:53:20 +0200

    new_task

--- src/pages/new_task/NewTask.js
+++ src/pages/new_task/NewTask.js
@@ -1,15 +1,46 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect}from 'react';
 import { Link } from 'react-router-dom';
 import './new_task.css'
 import Radio from "../../components/radio/radio";
 import Select from "../../components/select/select";
 import Button from "../../components/button/button";
+import axios from "axios";
 
 
 function NewTask() {
     const [radioVal1, setRadioVal1 ] = useState('');
     const [radioVal2, setRadioVal2 ] = useState('');
     const [selectVal, setSelectVal] = useState('');
+    const [DataPart, setDataPart]= useState([{id:"4", firstName:'part1', lastName:'part2'}]);
+    const [DataSq, setDataSq]= useState([{id:"5", firstName:'squad1', lastName:'squad2'}]);
+
+    useEffect(() => {
+        axios.get('/api/v1/searches/:id/coordinators/participants').then(function (response) {
+            setDataPart (response.data)
+            // [{id:"", firstName:'', lastName:''}]
+        }).catch(function (error) {
+            console.log('error')
+        })
+
+        axios.get('/api/v1/searches/:id/coordinators/squads').then(function (response) {
+            setDataSq(response.data)
+        }).catch(function (error) {
+            console.log('error')
+        })
+    })
+
+    let arr;
+
+    if (radioVal1==='поисковая группа'){
+        arr=DataSq
+    }else {
+        arr=DataPart
+    }
+
+    let options =arr.map (function(elem){ // {id:"", firstName:'', lastName:''}
+    return {label: elem.firstName+' '+elem.lastName,
+            value: elem.id} //{label: 'ФИО2', value:'value2'}
+    })
 
     return (
         <div className="New Task">
@@ -22,7 +53,7 @@ function NewTask() {
                     <Select
                             label={'Select '}
                             value={selectVal}
-                            options={[{label: 'ФИО1', value:'value1'}, {label: 'ФИО2', value:'value2'}]}
+                            options={options}
                             onChange={(val)=>{setSelectVal(val)}}>
 
                     </Select>
