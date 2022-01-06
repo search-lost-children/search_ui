0064c6fa835816d32fb2f0cd3c8eecfebe5f7c1e
web <web@training.workrocks.ua>
2022-01-06 20:55:27 +0200

    added post with verification

--- src/pages/login_pages/Login.js
+++ src/pages/login_pages/Login.js
@@ -17,8 +17,16 @@ function Login_page (){
     function user_login () {
 
             alert(' verification good')
-            axios.post('api/v1/users', user);
-            history.push('/')
+            axios.post('http://localhost:3000/api/v1/auth', user).then(
+                function (res) {
+                    if(res.data){
+                        sessionStorage.setItem('json', res.data);
+                        history.push('/');
+
+                    }
+                }
+            );
+
 
     }
     function user_verification (login,Password) {
@@ -35,9 +43,9 @@ function Login_page (){
         <div className="About">
 
             <h1>login page</h1>
-            <p>login</p>
+
             <Input type='login' label={'Login'} value={Login} onChange={(val)=> setLogin((val))}></Input>
-            <p>password</p>
+
             <Input type='password' label={'password'} value={Password} onChange={(val)=> setPassword((val))}></Input>
             <Button disabled = {user_verification()} value={'Log in'} onClick={()=>{user_login()}}></Button>
             <Button value={'registration'} onClick={()=>{history.push('/registration_page')}}></Button>
