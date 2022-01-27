ee6caebeda3708c46d5a5448ef9c28dc1b71cff9
web <web@training.workrocks.ua>
2022-01-27 20:54:05 +0200

    Almost done

--- src/components/taskString/taskString.js
+++ src/components/taskString/taskString.js
@@ -1,14 +1,14 @@
-import Button from "../button/button";
-import {useState} from "react";
+import Radio from "../radio/radio"
+import React, {useState} from "react";
 import {useRouteMatch} from "react-router-dom";
+import Radio from "../radio/radio";
 
 function TaskString(props) {
     let color
     let taskTypeLabel
     let match = useRouteMatch()
     const id = match.params.id
-    const [buttonStatus, setButtonStatus] = useState(false)
-    const [buttonValue, setButtonValue] = useState("Начать")
+    const [radioValue, setRadioValue] = useState("Показать данные на карте")
 
     function damnWTFIsThis(typeDep){
         let deps
@@ -28,23 +28,47 @@ function TaskString(props) {
 
     let taskRenderDeps = damnWTFIsThis(props.type)
 
-
+    function changelabel(value){
+        if(value === "Показать данные на карте"){
+            setRadioValue("Маршрут выведен на карту")
+        } else {
+            setRadioValue("Показать данные на карте")
+        }
+    }
     return (
         <div>
             <div>{props.label}</div>
             <div><font color={taskRenderDeps.color}>{taskRenderDeps.taskTypeLabel}</font></div>
             <div>
-                <Button
-                    onClick = {() => {
-                        // also тут должна быть функция, которая будет передавать координаты на карту для отрисовки маршрута
-                        setButtonValue("В процессе...")
-                        setButtonStatus(true)
+                <Radio
+                    value={radioValue}
+                    onChange={(val) => {
+                        setRadioVal(val)
                     }}
-                    value={buttonValue}
-                    disabled={buttonStatus}>
-                </Button>
+                    label={'Radio Button1'}
+                    name={'RadioButton'}
+                    checked={changeLabel()}/>
             </div>
         </div>)
 }
 
 export default TaskString
+
+
+
+
+
+
+
+
+
+
+{/*<Button*/}
+{/*    onClick = {() => {*/}
+{/*        // also тут должна быть функция, которая будет передавать координаты на карту для отрисовки маршрута*/}
+{/*        setButtonValue("В процессе...")*/}
+{/*        setButtonStatus(true)*/}
+{/*    }}*/}
+{/*    value={buttonValue}*/}
+{/*    disabled={buttonStatus}>*/}
+{/*</Button>*/}
