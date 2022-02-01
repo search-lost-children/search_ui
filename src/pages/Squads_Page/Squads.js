89207dabeec181dcfed6ee87a4a9236fe0a7c683
web <web@training.workrocks.ua>
2022-02-01 21:00:30 +0200

    squads

--- src/pages/Squads_Page/Squads.js
+++ src/pages/Squads_Page/Squads.js
@@ -5,11 +5,12 @@ import Select from "../../components/select/select";
 import Button from "../../components/button/button";
 import SquadsTable from "../../components/squadsTable/squadsTable";
 import GridTable from "@nadavshaar/react-grid-table";
+import axios from "axios";
 
 function SquadsSearch() {
-    const [selectVal1, setSelectVal1] = useState('');
-    const [selectVal2, setSelectVal2] = useState('');
+    const [selectVal, setSelectVal] = useState('');
     const [rows, setRows ] = useState([]);
+    const [DataPart, setDataPart]= useState([]);
     const columns = [
         {
             id: 1,
@@ -22,29 +23,49 @@ function SquadsSearch() {
             label: 'Actions'
         }]
 
+    useEffect(() => {
+        axios.get('/api/v1/searches/:id/participants').then(function (response) {
+            setDataPart (response.data)
+        }).catch(function (error) {
+            console.log('error')
+        })
+    }, [])
+
+        let arr = DataPart;
+        let options = arr.map (function(elem){
+            return {
+                label: elem.firstName + ' ' + elem.lastName,
+                value: elem.id
+            }
+        })
 
     // <SquadsTable coordinator={{firstName: '', lastName:''}} table={[]}/>
     return (<div className={'squadsPage'}>
             <h2>Отряды поиска ФИО</h2>
-            <div className="Title">
-                <div>Участник группы:  <Select
-                    label={'Select '}
-                    value={setSelectVal1()}
-                    options={options}
-                    onChange={(val)=>{setSelectVal1(val)}}>
-                </Select> </div>
-                <div className="Button1">
+        <div className="Title">
+                <div className="Select1">
+                    <Select
+                        label={'Участник группы: '}
+                        value={selectVal}
+                        options={options}
+                        onChange={(val)=>{setSelectVal(val)}}>
+                    </Select>
+                    <div className="Button1">
                     <Button value={'Добавить в группу'} onClick={() => {
                         alert('button is clicked')
                     }}></Button>
+                    </div>
                 </div>
-                <div>Координатор <Select
-                    label={'Select '}
-                    value={selectVal2}
+
+
+                <div className="Select2" >
+                    <Select
+                    label={'Координатор: '}
+                    value={selectVal}
                     options={options}
-                    onChange={(val)=>{setSelectVal2(val)}}>
+                    onChange={(val)=>{setSelectVal(val)}}>
                 </Select> </div>
-            </div>
+    </div>
             <div className={'table'}>
                 <GridTable columns={columns} rows={rows}></GridTable>
             </div>
@@ -54,7 +75,9 @@ function SquadsSearch() {
                 }}></Button>
             </div>
         </div>
+
     )
 }
+
 
 export default SquadsSearch;
